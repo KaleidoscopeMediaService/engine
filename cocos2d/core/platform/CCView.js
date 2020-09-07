e9362b169879af9b5131b766cc43e6ddfd88c864
murai-shu <murai@kms3.com>
2020-09-07 11:45:41 +0900

    nullチェックの追加

--- cocos2d/core/platform/CCView.js
+++ cocos2d/core/platform/CCView.js
@@ -1062,9 +1062,11 @@ cc.ContainerStrategy = cc.Class({
     _setupContainer: function (view, w, h) {
         var gameDiv = document.getElementById("GameDiv");
 
-        if (gameDiv.style.width.includes("px") && cc.sys.os !== cc.sys.OS_IOS) {
-          w = Number(gameDiv.style.width.match("(.*)(?=px)")[0]);
-          h = Number(gameDiv.style.height.match("(.*)(?=px)")[0]);
+        if (!CC_EDITOR && !!gameDiv) {
+            if (gameDiv.style.width.includes("px") && cc.sys.os !== cc.sys.OS_IOS) {
+                w = Number(gameDiv.style.width.match("(.*)(?=px)")[0]);
+                h = Number(gameDiv.style.height.match("(.*)(?=px)")[0]);
+            }
         }
 
         var locCanvas = cc.game.canvas;
